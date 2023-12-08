12b96f4c241e68040f18d40ed9d80e47934a2fca
Alysha Huot <alyshajhuot@gmail.com>
2023-12-08 13:24:09 -0700

    implement character counter functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,13 @@
-console.log("hello");
+$(document).ready(function() {
+
+  const $tweetText = $('#tweet-text');
+
+  // any time something is typed into text box, this function counts how many characters are currently typed, and subtracts it from 140 to show how many chars are left
+  $tweetText.on('input', function() {
+    let $counter = $(this).closest("form").find(".counter"); // goes up DOM to form, then down to .counter (and saves .counter to variable)
+    const text = $(this).val(); // this variable contains all text typed into textbox
+    const numOfChars = text.length; // find num of characters currently in text box
+    $counter.text(140-numOfChars); // change counter to equal 140-numOfChars
+  });
+
+});
