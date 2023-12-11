5ad77db7a5290ab30dd2150c765900ebf581b5b8
Alysha Huot <alyshajhuot@gmail.com>
2023-12-10 21:06:07 -0700

    create event listener for tweet-form submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -51,33 +51,36 @@ $(document).ready(function() {
   };
 
   renderTweets(data);
+
+  const $tweetForm = $('#tweet-form');
+
+  // event listener for tweet-form submit
+  $tweetForm.on('submit', function (event) {
+    event.preventDefault();
+    const $textarea = $(this).find('#tweet-text');
+    const serializedText = $textarea.serialize();
+    $.post("/tweets", serializedText, function () {
+    });
+  });
+
+  const loadTweets = function () {
+
+    $.get("/tweets", function(data) {
+      console.log(data);
+    });
+
+  };
+
+  loadTweets();
+
 });
 
-
-
-////////// TWEET DATA OBJECTS: /////////
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// event listener for tweet-form submit
+// const $tweetForm = $('#tweet-form');
+// $tweetForm.on('submit', function (event) {
+//   event.preventDefault();
+//   const $textarea = $(this).find('#tweet-text');
+//   const serializedText = $textarea.serialize();
+//   $.post("/tweets", serializedText, function () {
+//   });
+// });
